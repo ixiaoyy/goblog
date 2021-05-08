8935748daf1347148fab5d1b42e14389b3687dc7
root <root@jinli.etocrm.net>
2021-05-08 16:32:02 +0800

    集成ORM

---
+++ pkg/model/model.go
@@ -0,0 +1,30 @@
+package model
+
+import (
+	"goblog/pkg/logger"
+
+	"gorm.io/gorm"
+
+	// GORM 的 Mysql 数据库驱动导入
+	"gorm.io/driver/mysql"
+)
+
+// DB gorm.DB 对象
+var DB *gorm.DB
+
+// ConnectDB 初始化模型
+func ConnectDB() *gorm.DB {
+
+	var err error
+
+	config := mysql.New(mysql.Config{
+		DSN: "root:Ypy000923@tcp(47.92.162.137:3306)/goblog?charset=utf8&parseTime=True&loc=Local",
+	})
+
+	// 准备数据库连接池
+	DB, err = gorm.Open(config, &gorm.Config{})
+
+	logger.LogError(err)
+
+	return DB
+}
